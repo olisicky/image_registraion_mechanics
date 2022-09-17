93187b34f62ce60e7e8211f768b10f6cc86af862
olisicky <o.lisicky@seznam.cz>
2022-09-17 18:27:58 +0200

    higher differences in images and smoothing pyramids

--- deformation_field.py
+++ deformation_field.py
@@ -14,32 +14,11 @@ import matplotlib.gridspec as gridspec
 import skimage.io as sk
 from scipy import ndimage
 from pathlib import Path
+import cv2
+import os
 
 
-def show_images(fixed, moving, title = None, dpi = 40, margin = 0.05):
-    global ndf, ndm
-    # ndf = sitk.GetArrayFromImage(fixed)
-    # ndm = sitk.GetArrayFromImage(moving)
-    spacing = pixelsize
-    # print("Fixed image spacing is:", spacing)
-    margin = 0.05
-    figsize = (1 + margin) * fixed.shape[0] / dpi, (1 + margin) * fixed.shape[1] / dpi
-    extent = (0, fixed.shape[1]*spacing, fixed.shape[0]*spacing, 0)
-    figsize = (1 + margin) * fixed.shape[0] / dpi, (1 + margin) * fixed.shape[1] / dpi
-    fig = plt.figure(figsize=figsize, dpi=dpi)
-    ax = fig.add_axes([margin, margin, 1 - 2*margin, 1 - 2*margin])
-    plt.set_cmap("gray")
-    ax.imshow(fixed, extent = extent,alpha=1, cmap="jet")
-    ax.imshow(moving, extent = extent, alpha=0.5, cmap="Greens_r")
-    
-
-
-pixelsize = 1 
-sliceID = 500
-xRes = 800
-yRes = 800
-image_no = 2
-path = Path("./data")
+path = Path("./data/data_small")
 
 # =============================================================================
 # ========================== IMAGE REGISTRATION ===============================
@@ -70,7 +49,7 @@ parameterMap=sitk.ParameterMap()
 parameterMap["Registration"] = [registration[0]]
 parameterMap["UseDirectionCosines"] = ["true"]
 parameterMap['Transform'] = [transformation[0]]
-parameterMap["FinalGridSpacingInVoxels"] = ["128 128"]
+parameterMap["FinalGridSpacingInVoxels"] = ["64 64"]
 parameterMap["GridSpacingSchedule"] =  ["1", "1", "1"]
 outValue = 0
 parameterMap["DefaultPixelValue"] = ["{}".format(outValue)]   # default pixel value of pixels which come out of picture
@@ -85,7 +64,7 @@ parameterMap[ "MaximumNumberOfSamplingAttempts" ] = ["2048"]
 parameterMap["NumberOfGradientMeasurements"] = ["10"]
 parameterMap["NumberOfJacobianMeasurements"] = ["100000"]
 
-parameterMap["MaximumStepLength"] = ["{}".format(pixelsize)]
+parameterMap["MaximumStepLength"] = ["1"]
 parameterMap['MaximumNumberOfIterations'] = ["500", "500", "1000"]
 
 # Quasi Newton
@@ -97,19 +76,19 @@ parameterMap['MaximumNumberOfIterations'] = ["500", "500", "1000"]
 parameterMap["Interpolator"] = [interpolates[2]]
 parameterMap["BSplineInterpolationOrder"] = ["3"]
 # ___________________________ metrika ______________________________________
-parameterMap["Metric"] = [metrics[2]]
+parameterMap["Metric"] = [metrics[1]]
 # počet binů histogramů pro určení mutual informace
-parameterMap["NumberOfHistogramBins"] = ["64"]
-parameterMap["NumberOfSpatialSamples"] = ["100000"] #Number of spatial samples used to compute the mutual information in each resolution level.
+parameterMap["NumberOfHistogramBins"] = ["16"]
+parameterMap["NumberOfSpatialSamples"] = ["400000"] #Number of spatial samples used to compute the mutual information in each resolution level.
 parameterMap["NewSamplesEveryIteration"] = ["true"]    # refresh těchto vzorků kažkou iteraci
 parameterMap["ShowExactMetricValue"] = ["false"]
 parameterMap[ "MaximumNumberOfSamplingAttempts" ] = ["4"]
 # ____________________________ pyramidy __________________________________
-parameterMap["FixedImagePyramid"] = [pyramids[0]]
-parameterMap["MovingImagePyramid"] = [pyramids[1]]
+parameterMap["FixedImagePyramid"] = [pyramids[2]]
+parameterMap["MovingImagePyramid"] = [pyramids[3]]
 parameterMap["NumberOfResolutions"] = ["3"]
-parameterMap["FixedImagePyramidSchedule"] = ["8", "8", "4",  "4", "0", "0"]
-parameterMap["MovingImagePyramidSchedule"] = ["8", "8", "4",  "4", "0", "0"]
+parameterMap["FixedImagePyramidSchedule"] = ["4", "4", "2",  "2", "0", "0"]
+parameterMap["MovingImagePyramidSchedule"] = ["4", "4", "2",  "2", "0", "0"]
 
 parameterMap["WriteIterationInfo"] = ["true"]
 parameterMap["ImageSampler"] = ["Random"]
@@ -123,33 +102,44 @@ parameterMap["HowToCombineTransforms"] = ["Compose"]
 # =============================================================================
 # ========================= HLAVNÍ CYKLUS REGISTRACE ==========================
 # =============================================================================
-initial = path / "deformed_0.tif"
-initial = sk.imread(initial)
-initial=initial[260:550, 230:550]
-# vytvoření hlavních matic, kam se mi budou ukládat výsledky z registračního procesu
-deformed_images = np.empty([image_no, initial.shape[0], initial.shape[1]])
-strain_fields_X = np.empty([image_no+1, initial.shape[0], initial.shape[1]])
-strain_fields_Y = np.empty([image_no+1, initial.shape[0], initial.shape[1]])
-strain_fields_X[0,:,:] = np.zeros((initial.shape[0], initial.shape[1]))
-strain_fields_Y[0,:,:] = np.zeros((initial.shape[0], initial.shape[1]))
-Deformation_field_X = np.empty([image_no+1, initial.shape[0], initial.shape[1]])
-Deformation_field_Y = np.empty([image_no+1, initial.shape[0], initial.shape[1]])
-Deformation_field_X[0,:,:] = np.zeros((initial.shape[0], initial.shape[1]))
-Deformation_field_Y[0,:,:] = np.zeros((initial.shape[0], initial.shape[1]))
+x0, x1, y0, y1 = 200, 600, 150, 600 
 
-for i in range (0, image_no,1):
-    fixed = path / "deformed_{}.tif".format(i+1)
-    fixed_read = sk.imread(fixed)
-    fixed_read=fixed_read[260:550, 230:550]
+def load_images(path):
+    images =  []
+    for file in os.listdir(path):
+        if file.endswith(".tif"):
+            images.append(file)
+    print(images)
+    shape = sk.imread(path / images[0]).shape    # get shape of initial image
+    stack = np.empty((len(images), shape[0], shape[1]))
+    for i, image in enumerate(images):
+        stack[i, :, :] = sk.imread(path / image)
+    return stack
+
+images = load_images(path)[:, y0:y1, x0:x1]
+
+# vytvoření hlavních matic, kam se mi budou ukládat výsledky z registračního procesu
+deformed_images = np.empty([images.shape[0], images.shape[1], images.shape[2]])
+strain_fields_X = np.empty([images.shape[0] + 1, images.shape[1], images.shape[2]])
+strain_fields_Y = np.empty([images.shape[0] + 1, images.shape[1], images.shape[2]])
+strain_fields_X[0,:,:] = np.zeros((images.shape[1], images.shape[2]))
+strain_fields_Y[0,:,:] = np.zeros((images.shape[1], images.shape[2]))
+Deformation_field_X = np.empty([images.shape[0] + 1, images.shape[1], images.shape[2]])
+Deformation_field_Y = np.empty([images.shape[0] + 1, images.shape[1], images.shape[2]])
+Deformation_field_X[0,:,:] = np.zeros((images.shape[1], images.shape[2]))
+Deformation_field_Y[0,:,:] = np.zeros((images.shape[1], images.shape[2]))
+
+for i, image in enumerate(images):
+    if i == images.shape[0] - 1:
+        break
     
-    moving = path / "deformed_{}.tif".format(i)
-    moving_read = sk.imread(moving)
-    moving_read=moving_read[260:550, 230:550]
+    fixed = images[i + 1, :, :]
+    moving = image
     
-    fixed_img = sitk.GetImageFromArray(fixed_read)
-    moving_img = sitk.GetImageFromArray(moving_read)
-    fixed_img.SetSpacing([pixelsize,pixelsize, pixelsize])
-    moving_img.SetSpacing([pixelsize,pixelsize, pixelsize])
+    fixed_img = sitk.GetImageFromArray(fixed)
+    moving_img = sitk.GetImageFromArray(moving)
+    fixed_img.SetSpacing([1, 1, 1])
+    moving_img.SetSpacing([1, 1, 1])
     fixed_img.SetOrigin([0,0,0])
     moving_img.SetOrigin([0,0,0])
     
@@ -178,8 +168,8 @@ for i in range (0, image_no,1):
 # =============================================================================
 # ===================== TVORBA BODŮ PRO TRANSFORMACI ==========================
 # =============================================================================    
-    x = np.arange(0, (moving_read).shape[1])
-    y = np.arange(0, (moving_read).shape[0])
+    x = np.arange(0, (moving).shape[1])
+    y = np.arange(0, (moving).shape[0])
     X,Y = np.meshgrid(x,y)
     X = X + Deformation_field_X[i,:,:]    # první deformation field bude O pro oba směry
     Y = Y + Deformation_field_Y[i,:,:]
@@ -216,64 +206,94 @@ for i in range (0, image_no,1):
 
 # ======================= Odečtení dat po registraci ==========================
 
-    x_dir = np.asarray(deformationField["x_deformation"], dtype=float)* (-1)
+    x_dir = np.asarray(deformationField["x_deformation"], dtype=float) * (-1)
     y_dir = np.asarray(deformationField["y_deformation"], dtype=float) * (-1)
 
-    Grid_x = (x_dir.reshape((-1, fixed_read.shape[1]))).astype(np.float)
-    Grid_y = (y_dir.reshape((-1, fixed_read.shape[1]))).astype(np.float)
+    Grid_x = (x_dir.reshape((-1, fixed.shape[1]))).astype(np.float)
+    Grid_y = (y_dir.reshape((-1, fixed.shape[1]))).astype(np.float)
 
 # zde by se mělo vytvořit pole posuvů, které bude stále narůstat a bude dále
 # využíváno pro tvoření nových bodů v další iteraci
     Deformation_field_X[i+1,:,:] = Deformation_field_X[i,:,:] + Grid_x
     Deformation_field_Y[i+1,:,:] = Deformation_field_Y[i,:,:] + Grid_y
     
-    Grid_x_1 = ndimage.gaussian_filter(Grid_x, 20)
-    Grid_y_1 = ndimage.gaussian_filter(Grid_y, 20)
+    print(f'{i}th image is done')
+
+def calc_strain(displacement_X, displacement_Y):
+    # nejprve vyhlazení posuvů
+    smoothed_XX = np.empty([displacement_X.shape[0], displacement_X.shape[1], displacement_X.shape[2]])
+    smoothed_YY = np.empty([displacement_Y.shape[0], displacement_Y.shape[1], displacement_Y.shape[2]])
+    E_xx = np.empty([displacement_X.shape[0], displacement_X.shape[1], displacement_X.shape[2]])
+    E_yy = np.empty([displacement_Y.shape[0], displacement_Y.shape[1], displacement_Y.shape[2]])
+    E_xy = np.empty([displacement_Y.shape[0], displacement_Y.shape[1], displacement_Y.shape[2]])
+
+    i = 0
+    for dispX, dispY in zip(displacement_X, displacement_Y):
+            # vyhlazení posuvů kvůli šumu pomocí Gaussian filtru
+        #smoothed_XX[i,:,:] = ndimage.gaussian_filter(disp_x, 10)
+        #smoothed_YY[i,:,:] = ndimage.gaussian_filter(disp_y, 10)
+        #smoothed_XX[i, :,:] = sgolay2d(dispX, window_size = 5, order = 1, derivative = None)
+        #smoothed_YY[i, :,:] = sgolay2d(dispY, window_size = 5, order = 1, derivative = None)
+        smoothed_XX[i,:,:] = dispX
+        smoothed_YY[i,:,:] = dispY
+        dudx = np.gradient(smoothed_XX[i,:,:], edge_order = 2, axis = 1)   
+        dvdy = np.gradient(smoothed_YY[i,:,:], edge_order = 2, axis = 0)
+        dudy = np.gradient(smoothed_XX[i,:,:], edge_order = 2, axis = 0)
+        dvdx = np.gradient(smoothed_YY[i,:,:], edge_order = 2, axis = 1)
+        E_xx[i,:,:] = 1/2*(2*dudx + (dudx)**2 + (dvdx)**2) 
+        E_yy[i,:,:] = 1/2*(2*dvdy + (dudy)**2 + (dvdy)**2)
+        E_xy[i,:,:] = 0.5*(dudy + dvdx + dudy*dudy + dvdx*dvdy)
+        i = i+1
+    return E_xx, E_yy, E_xy
+
+
+E_xx, E_yy, E_xy = calc_strain(Deformation_field_X, Deformation_field_Y) 
+
+
+
+def for_imageJ(image1, image2):
+    """ První obrázek je originál nahraný a měl by být i+1 oproti obrazu dva, který
+        je zase zdeformovaný pomocí IR. Při kontrole v ImageJ originální deformace bych
+        měl vždy kontrolovat právě image1 s obrazem o číslo menší. Jelikož i-1 deformuji."""
+    #img1 = image1    # zde musí být i+1 pro slazené obrazy. Pro zobrazení předchozího rozdílu je potřeba zobrazit i
+    #img1 = sk.imread(img1)
+    #img1=img1[y0:y1, x0:x1]
+    #img2 = image2    # zde musí být i+1 pro slazené obrazy. Pro zobrazení předchozího rozdílu je potřeba zobrazit i
+    #img2 = sk.imread(img2)
+    #img2=img2[y0:y1, x0:x1]
     
-    strain_x = np.gradient(Grid_x_1, 1, axis = 1)
-    strain_y = np.gradient(Grid_y_1, 1, axis = 0)
-
-    strain_fields_X[i+1,:,:] = strain_fields_X[i,:,:] + strain_x
-    strain_fields_Y[i+1,:,:] = strain_fields_Y[i,:,:] + strain_y
-    print("One is done")
-
-
-# for k in range(image_no+1):
-#     fig, (ax,bx, cx) = plt.subplots(3,1, figsize=(5,10))
-#     if k == 0:
-#         ax.imshow(initial,cmap="gray")
-#     else:
-#         try:
-#             ax.imshow(deformed_images[k,:,:], cmap="gray")
-#         except:
-#             pass    
-        
-#     crop = strain_fields_X.copy()   # jinak se to přepisuje!! Bída
-#     crop  = crop[k,:,:]
-#     crop[0:100, 0:crop.shape[1]] = np.nan    # pozor, zde je to už ořezaný kus, takže chce ladit na něj
-#     crop[590:crop.shape[0], 0:crop.shape[1]] = np.nan
-#     crop[0:crop.shape[0], 0:100]  = np.nan
-#     crop[0:crop.shape[0], 540:crop.shape[1]] = np.nan                       
-#     # fig = ax.imshow(strain_fields_X[3,215:390,518:830], cmap="jet" ,alpha = 1)
-#     # fig = ax.imshow(crop, cmap="jet" ,alpha = 1)
+    cv2.imwrite("for_imageJ_0.tif", image1)
+    cv2.imwrite("for_imageJ_1.tif", image2)
+    #cv2.imwrite("for_imageJ_1.tif", img2)
     
-#     bx.imshow(initial, alpha = 1, cmap="gray")
-#     fig = bx.imshow(crop, cmap="jet" ,alpha = 0.8)
-#     plt.colorbar(fig, ax = bx)
+for_imageJ(images[1, :,:], deformed_images[0,:,:])
 
-#     cx.set_xlim(0,30)
-#     cx.set_ylim(0,8)
-#     cx.set_xlabel("image no.")
-#     cx.set_ylabel("Force F [N]")
-#     cx.plot(np.linspace(0,k,k), forceX[0:k], label = "Applied force [N]")
-#     plt.legend()
-#     plt.subplots_adjust(bottom=0.15)
-#     plt.savefig("strainX_{}".format(k+1))
+
+def plot_sample(characteristics, limit):
+    """"Tato funkce vykreslí výsledky zvolené veličiny na vzorku nedeformovaném,
+        který je oříznut pomocí masky. Maska byla vytvořena pomocí imageJ. Limit
+        je zde jenom z toho důvodu, že jsou tam místy lokální extrémy bodové, které
+        rozhodí tu škálu. """
+    mask = sk.imread("Mask.png")
+    _, mask = cv2.threshold(mask[y0:y1, x0:x1], thresh=180, maxval=255, type=cv2.THRESH_BINARY_INV)
+    masked = np.ma.masked_array(characteristics, ~mask)
+    masked[masked > limit] = np.nan 
+    plt.figure()
+    plt.imshow(images[0, :,:], cmap = "gray")
+    im = plt.imshow(masked, alpha = 0.7, cmap = "jet")
+    plt.xticks([])
+    plt.yticks([])
+    plt.colorbar(im)
+    return masked
+
+mask = plot_sample(E_yy[6,:,:], 0.8)
+
+
 
 # ======================== Kontrola procedené deformace =======================
 img1 = path / "deformed_1.tif"    # zde musí být i+1 pro slazené obrazy. Pro zobrazení předchozího rozdílu je potřeba zobrazit i
 img1 = sk.imread(img1)
-img1=img1[260:550, 230:550]
+img1=img1[x0:x1, y0:y1]
 img2 = deformed_images[1,:,:]   # zde je i
 show_images(img2, img1)
 
